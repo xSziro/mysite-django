734db2d6c255c7ed0a3380212bf6f9dca8f16e89
xSziro <milybukowiecki@gmail.com>
2019-11-25 18:38:38 +0100

    models created

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_lenght = 400)
-    publication_date = models.DateTimeField('date of publishing')
+class User(models.Model):    
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 15)
+    password = models.DateTimeField(max_length = 32)
+    email = models.CharField(max_length = 30)
+    
 
-class choice(models.Model):
-    question = models.ForeingKey(Question,on_delete = models.CASCADE)
-    choice_text = models.CharField(max_lenght=200)
-    votes = models.IntegerFields(default=0)
+
+    
+
+class Game(models.Model):
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 30)
+    country = models.CharField(max_length = 30)
+    
+class Comment(models.Model):
+    user_id =  models.Foreign(User,on_delete = models.Cascade)
+    id =  models.Foreign(User,on_delete = models.Cascade)
+    text = models.CharField(max_length = 400)
+
+class Vote(models.Model):
+    user_id =  models.Foreign(User,on_delete = models.Cascade)
+    game_id =  models.Foreign(User,on_delete = models.Cascade)
+    vote = models.IntegerField(default = 0)
--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 # Create your views here.
 
+
 def index(request):
+    all = User.object.all()
     return HttpResponse("Pierwsza apk django")
+    
+
+def test(request):
+    p = User(name = 'user1',password = 'abcd1234', email = 'www2www@w.ww')
+    p.save()
+    return HttpResponse("test")
+
